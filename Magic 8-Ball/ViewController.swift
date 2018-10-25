665c06f426cb614bcef6246f376c1454d7834df9
Lucia Reynoso <rvaicul@gmail.com>
2018-10-25 11:07:24 -0700

    basic functionality

---
+++ Magic 8-Ball/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Magic 8-Ball
+//
+//  Created by Lucia Reynoso on 10/25/18.
+//  Copyright © 2018 Lucia Reynoso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    let answers = ["Yes, definitely", "It is certain", "Without a doubt", "Yes", "Most likely", "Sure, why not?", "Same", "Tell me more", "Out to lunch", "Reply hazy, try again", "Ask again later", "The cake is a lie", "42", "TMI", "Very doubtful", "Don't count on it", "My reply is no", "Absolutely not"]
+    @IBOutlet weak var shakeButton: UIButton!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func generateAnswer() {
+        answerLabel.text = answers.randomElement()
+    }
+
+    @IBAction func shakeButtonTapped(_ sender: Any) {
+        print("Shake it like a polaroid picture!")
+        
+        generateAnswer()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        guard motion == .motionShake else { return }
+        
+        generateAnswer()
+    }
+    
+}
+
